f453349b34987f85be4c611f46ee71a411fed30a
2003Rajansahni <rajansahni2003@gmail.com>
2025-04-23 23:53:46 +0530

    Changing frontend file

--- mern-auth-frontend/src/App.js
+++ mern-auth-frontend/src/App.js
@@ -1,38 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// import Login from './components/Login';
-// import Register from './components/Register';
-// import StudentDashboard from './components/StudentDashboard';
-// import AdminDashboard from './components/AdminDashboard';
-// import AddQuestion from './components/AddQuestion';
-// import GetQuestions from './components/GetQuestion';
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/studashboard" element={<StudentDashboard />} />
-//         <Route path="/admdashboard" element={<AdminDashboard />} />
-//         <Route path="/add-question" element={<AddQuestion />} />
-
-
-//         <Route path="/get-question" element={<GetQuestions />} />
-
-//         <Route path="/" element={<Navigate to="/login" />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
